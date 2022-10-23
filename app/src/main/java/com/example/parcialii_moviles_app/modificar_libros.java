71814567407188d7cd0cda819e0e2d49354f6371
J05K4 <diazmoranjosue@gmail.com>
2022-10-23 18:23:04 -0500

    esaxamen
    actualizacion ultima

---
+++ app/src/main/java/com/example/parcialii_moviles_app/modificar_libros.java
@@ -0,0 +1,14 @@
+package com.example.parcialii_moviles_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modificar_libros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_libros);
+    }
+}
---
+++ app/src/main/java/com/example/parcialii_moviles_app/registrar_libros.java
@@ -0,0 +1,14 @@
+package com.example.parcialii_moviles_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrar_libros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_libros);
+    }
+}
